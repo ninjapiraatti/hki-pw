fec460dbd517711a186414f893f949fbae41bba7
Tuomas Louekari <tuoppil@gmail.com>
2024-08-09 11:50:34 +0300

    Add uuid and a simple link for character creation

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,1 +1,13 @@
-<template></template>
+<template>
+	<RouterLink :to="characterLink" tag="button">Make a character</RouterLink>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue"
+import { v4 as uuidv4 } from "uuid"
+
+const characterId = uuidv4()
+const characterLink = computed(() => {
+	return `/characters/${characterId}`
+})
+</script>
