96b661e971d4abd03cc335c05bfb95e56f242b66
Tuomas Louekari <tuoppil@gmail.com>
2024-08-15 14:14:59 +0300

    Encode page data to json

--- site/templates/character.php
+++ site/templates/character.php
@@ -2,8 +2,22 @@
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
-$url = $input->url(); 
-  $url = $sanitizer->entities($url); // entity encode for output
-  echo "You accessed this page at: $url";
+
+foreach($page->images as $image) {
+  $imageUrl = $image->url;
+}
+$data = [
+  'title' => $page->title,
+  'body' => $page->body,
+  'images' => $imageUrl,
+  'attribute_strength' => $page->attribute_strength,
+  'attribute_perception' => $page->attribute_perception,
+  'attribute_endurance' => $page->attribute_endurance,
+  'attribute_charisma' => $page->attribute_charisma,
+  'attribute_intelligence' => $page->attribute_intelligence,
+  'attribute_agility' => $page->attribute_agility,
+  'attribute_luck' => $page->attribute_luck,
+];
+echo wireEncodeJSON($data);
 
 ?>
